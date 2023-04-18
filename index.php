b7889a93901920726e278fd478e8afd96470b57d
rackys2000 <rackysatioquia35@gmail.com>
2023-04-18 13:52:00 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>One Tap Garage</title>
+        <?php include 'bootstrap/styles.php'; ?> 
+        <style>
+            .card{
+                background-color: #FF7B54;
+            }
+            .orange-section{
+                background-color: #FF7B54;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary py-3" data-bs-theme="dark">
+                <div class="container">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <img class="" style="width: 100px;" src="images/307586122_639980424527457_6296091053058125249_n.png" alt="onetapgarage" />
+                        <ul class="navbar-nav ms-auto gap-4 align-items-center gap-4">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="#"><h5>Our Clients</h5></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><h5>Services Offered</h5></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><h5>About Us</h5></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><h5>Contact Us</h5></a>
+                            </li>
+                            <li class="nav-item">
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-primary text-white" data-bs-toggle="modal" data-bs-target="#loginModal">
+                                    <span class="h5">Book Now</span>
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <section class="mb-5 pb-5">
+            <div class="d-flex justify-content-between">
+                <div class="d-flex flex-column gap-4 justify-content-center mx-5 px-5">
+                    <h1 class="fw-bold">Service Beyond the Standards</h1>
+                        <span class="fw-bold text-secondary h4">Unlock the happiness of driving with our exceptional automotive engineering and design.</span>
+                    <div>
+                        <button class="p-3 btn btn-primary text-white">Learn More</button>
+                    </div>
+                </div>
+                <img src="images/Group 1.png" alt="Image" />
+            </div>
+        </section>
+        <section class="py-5 container">
+            <div class="text-center">
+                <h1 class="fw-bold text-decoration-underline">Service Beyond the Standards</h1>
+            </div>
+            <div class="row g-2 mt-5">
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Aircon Cleaning</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">ATF Fluid Flushing</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Battery Charging</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Brake Clean & Adjust</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center ">
+                            <h3 class="text-body-secondary">Brake Fluid Flash</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center text-center">
+                            <h3 class="text-body-secondary">Condenser Replacement</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Change Oil</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Wheel Balancing</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Change Air Filter</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Change Bulb</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Change Cabin</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Change Brake Pad</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Change Spark Plug</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Fan Replacement</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Freon Recharge</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Radiator Flush</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card ratio ratio-4x3">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <h3 class="text-body-secondary">Tire Rotation</h3>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <section class="py-5 container">
+            <div class="text-center mb-5">
+                <h1 class="fw-bold text-decoration-underline">About Us</h1>
+            </div>
+            <div class="row gy-5 gap-5">
+                <div class="col-12">
+                    <img src="/images/Group 12.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-12">
+                    <img src="/images/Group 13.png" class="img-fluid" alt="">
+                </div>
+            </div>
+        </section>
+        <section class="py-5 container-fluid orange-section">
+            <div class="mb-5">
+                <h1 class="fw-bold text-decoration-underline text-white text-center">Contact Us</h1>
+                <div class="container my-5">
+                    <div class="row">
+                        <div class="col-6 px-5">
+                            <form method="post">
+                                <div class="mb-3">
+                                    <label for="" class="form-label h5 text-white">Full Name</label>
+                                    <input type="text" class="form-control form-control-lg" id="" aria-describedby="emailHelp" />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label h5 text-white">Email Address</label>
+                                    <input type="email" class="form-control form-control-lg" id="" aria-describedby="emailHelp" />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label h5 text-white">Mobile No</label>
+                                    <input type="email" class="form-control form-control-lg" id="" aria-describedby="emailHelp" />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label h5 text-white">Message</label>
+                                    <textarea class="form-control" name="" id="" cols="30" rows="5"></textarea>
+                                </div>
+                                    <button type="submit" class="btn btn-dark btn-lg w-100">Submit</button>
+                            </form>
+                        </div>
+                        <div class="col-6">
+                            <div class="">
+                                <img src="/images/Mask group.png" alt="">
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="position-relative">
+            <div class="position-absolute top-0 start-50 translate-middle-x p-4 z-1 h-100 d-flex flex-column justify-content-center text-center gap-3" >
+                <h2 class="text-white">Do you want to be with us?</h2>
+                <span class="text-white">Choosing to be a partner means putting your trust in someone else and building a relationship based on mutual respect, honesty, and reliability. At our core, we believe that trust is the foundation of any successful partnership. That's why we're committed to working with our partners to build long-lasting relationships that stand the test of time.</span>
+                <div class="d-inline">
+                    <a href="http://" class="btn btn-primary btn-lg px-5">Join Now</a>
+                </div>
+            </div>
+            <img src="images/Group 14.png" class="img-fluid" alt="">
+        </section>
+        <footer class="bg-dark ">
+            <div class="container p-5">
+                <div class="row">
+                    <div class="col-4">
+                        <img class="" style="width: 220px;" src="images/307586122_639980424527457_6296091053058125249_n.png" alt="onetapgarage" />
+                    </div>
+                    <div class="col-4">
+                        <h5 class="text-white mb-3">Category</h5>
+                        <div class="text-white d-flex flex-column ">
+                            <span>Service Offered</span>
+                            <span>About Us</span>
+                            <span>Contact us</span>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <h5 class="text-white mb-3">Follow Us</h5>
+                        <div class="text-white d-flex flex-column ">
+                            <span>Twitter</span>
+                            <span>Facebook</span>
+                            <span>Instagram</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center py-3">
+                <small class="text-white-50">Copyright 2023. All right reserved.</small>
+            </div>
+        </footer>
+    </body>
+</html>
