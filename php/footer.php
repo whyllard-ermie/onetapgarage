5bf0b06be3e2c4283233c6c51bd62b8c3357b5bf
orland04 <orlandbalana21@gmail.com>
2023-04-20 23:54:07 +0800

    last update

--- php/footer.php
+++ php/footer.php
@@ -15,9 +15,9 @@
                     <div class="col-4">
                         <h5 class="text-white mb-3">Follow Us</h5>
                         <div class="text-white d-flex flex-column ">
-                            <span>Twitter</span>
-                            <span>Facebook</span>
-                            <span>Instagram</span>
+                            <span><a href="https://twitter.com/OneTap2023">Twitter</a></span>
+                            <span><a href="https://www.facebook.com/profile.php?id=100091677832582">Facebook</a></span>
+                            <span><a href="https://www.instagram.com/accounts/onetap/?next=%2F">Instagram</a></span>
                         </div>
                     </div>
                 </div>
