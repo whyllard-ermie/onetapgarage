a9035f834e6d8908df6147f3f925477da5dcb145
orland04 <orlandbalana21@gmail.com>
2023-04-25 22:51:08 +0800

    latest

--- client/client.php
+++ client/client.php
@@ -1,4 +1,11 @@
+<?php
 
+session_start();
+
+
+$fname= $_POST['fname'];
+
+?>
 <html lang="en">
 <link rel="stylesheet" href="../css/style.css">
 <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
@@ -17,7 +24,7 @@
                 <h6>Fill all form field to go to next step</h6>
                 <div class="row">
                     <div class="col-md-12 mx-0">
-                        <form id="msform" action="try.php" method="post"  >
+                        <form id="msform" action="" method="post" onsubmit="openModal()"  >
                             <!-- progressbar -->
                             <ul id="progressbar">
                                 <li class="active" id="account"><strong>Appoitnment</strong></li>
@@ -29,7 +36,7 @@
                             <fieldset id="f1">
                                 <div class="form-card">
                                     <label for="fname">First Name</label>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<label for="mobile" id="mobile">Mobile</b></label><br>
-                                        <input type="text" id="fname" name="fname">
+                                        <input type="text" id="fname" name="fname"/>
                                             <input type="text" id="mobilein" name="mobile"><br>
                                     <label for="lname">Last Name</label>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<label for="address">Address</label><br>                                   
                                         <input type="text" id="lnameinput" name="lname" placeholder=""/><br>    
@@ -120,7 +127,7 @@
                                     </div>
                                 </div>
                                 <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                                <input type="button"  class="next action-button" name="make_payment"  value="Submit" data-toggle="modal" data-target="#reviewModal" />
+                                <input type="submit"  class="next action-button" name="make_payment"  />
                             </fieldset>
                             <fieldset>
                                 <div class="form-card">
@@ -133,8 +140,6 @@
                                     </div>
                                 </div>
                                 <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
-                                <input type="button"  class="next action-button" name="make_payment"  value="Submit" data-toggle="modal" data-target="#reviewModal" />
-                            </fieldset>
                         </form>
                        
                     </div>
@@ -144,4 +149,55 @@
     </div>
 </div>
 <?php include '../customer/footer.php'?>
-<?php require_once "reviewmodal.php"; ?>
+<!-- modal start -->  
+<div class="modal fade" id="reviewModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header border-bottom-0">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-title">
+          <h6><b>Please review your details</b></h6>
+        </div>
+        
+        <div class="d-flex flex-column">
+          <form>
+            <div class="form-group">
+            <?php
+echo "The name you entered is $fname";
+?>
+            </div>
+            <div class="form-group">
+              <div class="icon">
+            </div>
+            </div>
+            <br><br>
+            <button type="submit" class="btn btn-info btn-block btn-round" name="login" value="Log In"><b>Submit</b></button>
+          </form>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- partial -->
+
+  
+  </article>
+ </main>
+ 
+  <!-- jQuery -->
+  <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
+  <!-- Popper JS -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js'></script>
+  <!-- Bootstrap JS -->
+  <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'></script>
+   <!-- Custom Script -->      
+  <script  src="../js/review.js"></script>
+  <!-- modal end-->    
+
+  
+
--- js/review.js
+++ js/review.js
@@ -1,13 +1,6 @@
-// show loginmodal start  here-------
-$(document).ready(function() {             $('#reviewModal').modal('hide');
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
-});
-// END
- // clear modal input on close start here-------
 
- $('#reviewModal').on('hidden.bs.modal', function () {
-    $(this).find('form').trigger('reset');
-  })
-  // END
+
+  $('#msform').on('submit', function(e){
+    $('#reviewModal').modal('show');
+    e.preventDefault();
+  });
