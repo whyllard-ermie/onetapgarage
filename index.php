e0d35daf7a49bd596cdbb047ab42d5b717915fe7
Whyllard Ermie <72491828+whyllard-ermie@users.noreply.github.com>
2023-04-27 23:06:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'php/header.php';?>
+<?php include './php/header.php';?>
         <body class="body" id="body">
         <section class="mb-5 pb-5" >
             <div class="d-flex justify-content-between">
